2957da0b64a3ad264d21ad676a443c82c2ae7818
Rubén Bermejo Romero <mejoruben@gmail.com>
2020-12-27 19:31:10 +0100

    Added support for upper case

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # decoder thing
 
-idk im bored and i don't know how to have fun instead here goes me trying out github pages haha yes
+idk im bored and i don't know how to have fun during my holidays so instead here goes me trying out github pages haha yes
--- resources/logic.js
+++ resources/logic.js
@@ -1,36 +1,51 @@
-
-let alphabet = [];
-let reverseAlphabet = [];
+var orderedAlphabet;
+var reversedAlphabet;
 
 $(document).ready(() => {
-    for (let i = 0; i < 26; i++) {
-        alphabet.push(String.fromCharCode(i + 97))
-    }
-
-    reverseAlphabet = [...alphabet];
-    reverseAlphabet.reverse();
+    setupArrays();
 });
 
 function process() {
-    let action = $('input[name=actionToPerform]').val()
+    let action = $('input[name=actionToPerform]').val();
 
     if (action === "encode") {
-        $("#textOutput").val(processText(alphabet, reverseAlphabet, $("#textInput").val()));
+        $("#textOutput").val(processText(orderedAlphabet, reversedAlphabet, $("#textInput").val()));
     } else if (action === "decode") {
-        $("#textOutput").val(processText(reverseAlphabet, alphabet, $("#textInput").val()));
+        $("#textOutput").val(processText(reversedAlphabet, orderedAlphabet, $("#textInput").val()));
     }
 }
 
 function processText(lookUp, reference, input) {
     let result = "";
 
-    for (let letter of input.toLowerCase()) {
+    for (let letter of input) {
         if (lookUp.includes(letter)) {
-            result += reference[lookUp.indexOf(letter)]
+            result += reference[lookUp.indexOf(letter)];
         } else {
             result += letter;
         }
     }
 
     return result;
+}
+
+function setupArrays() {
+    let alphabet = [];
+    let upperCaseAlphabet = [];
+    let reverseAlphabet = [];
+    let reverseUpperCaseAlphabet = [];
+    
+    for (let i = 0; i < 26; i++) {
+        alphabet.push(String.fromCharCode(i + 97));
+        upperCaseAlphabet.push(String.fromCharCode(i + 65));
+    }
+
+    reverseAlphabet = [...alphabet];
+    reverseUpperCaseAlphabet = [...upperCaseAlphabet];
+    reverseAlphabet.reverse();
+    reverseUpperCaseAlphabet.reverse();
+
+    orderedAlphabet = alphabet.concat(upperCaseAlphabet);
+    reversedAlphabet = reverseAlphabet.concat(reverseUpperCaseAlphabet);
+
 }
